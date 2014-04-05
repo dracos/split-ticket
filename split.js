375389a4102d8b5dab8dbe731a4b979a025a526c
Matthew Somerville <matthew-github@dracos.co.uk>
2014-04-05 23:52:05 +0100

    Fetch departure times, start of time-based picking of ticket.

--- split.js
+++ split.js
@@ -36,57 +36,97 @@ function Pfetch(url) {
 }
 
 function price(n) {
-    return printf('%.2f', n/100);
+    return printf('£%.2f', n/100).replace('.00', '');
+}
+
+
+function test_restriction(code, time) {
+    if ([ '2V', 'G1', 'BX', 'GF' ].indexOf(code)>-1 && time >= '09:30') {
+        return true;
+    }
+    if ([ 'W3' ].indexOf(code)>-1 && time >= '09:00') { // AND THE 08:59 TILEHURST TO OXFORD!
+        return true;
+    }
+    if ([ 'W4' ].indexOf(code)>-1 && time >= '08:45') { // Additionally valid on the 08:41 departure from Swindon to Cheltenham Spa. 
+        return true;
+    }
+    return false;
 }
 
 function parse_fare(from, to) {
     return Pfetch('http://api.brfares.com/querysimple?orig=' + from + '&dest=' + to)
     .then(function(body) {
-        var b = body.fares.filter(function(s) {
-            var test = s.ticket.code.match(/SVR|SOR|CDR|SDR|CDS|SDS|SOS/);
-            return test;
+        var b = body.fares.filter(function(s) { return s.ticket.code.match(/SVR|SOR|CDR|SDR|CDS|SDS|SOS/); });
+
+        function fare_list(s) {
+            var o = price(s.adult.fare);
+            o += ' (' + prettify(s.ticket.name);
+            if (s.route.name != 'ANY PERMITTED') o += ', ' + prettify(s.route.name)
+            if (s.restriction_code != '  ') o += ', ' + s.restriction_code;
+            o += ')';
+            return o;
+        }
+
+        var returns = body.fares.filter(function(s) {
+            var ret = s.ticket.code.match(/SVR|SOR/);
+            if (store.day) {
+                ret = ret || s.ticket.code.match(/CDR|SDR/);
+            }
+            return ret;
         });
-        var by_type = {};
-        b.forEach(function(s) { by_type[s.ticket.code] = s; });
-        var best = by_type.SVR || by_type.SOR;
+
+        var best;
+        returns.forEach(function(r){
+            if (!best) { best = r; return; }
+            if (r.adult.fare < best.adult.fare) {
+                if (r.ticket.code == 'SOR' || r.ticket.code == 'SDR') {
+                    best = r; return;
+                }
+                if (best.ticket.code == r.ticket.code && r.restriction_code == best.restriction_code) {
+                    best = r; return;
+                }
+                if (from in store.departure_times) {
+                    if (test_restriction(r.restriction_code, store.departure_times[from])) {
+                        best = r; return;
+                    }
+                }
+            }
+        });
+
         var double = false;
-        if (store.day) {
-            var best_day = by_type.CDR || by_type.SDR;
-            if (!best || (best_day && best_day.adult.fare < best.adult.fare)) {
-                best = best_day;
+        if (best) {
+            process.stdout.write(returns.map(fare_list).join(' | ') + ' ');
+        } else {
+            var singles = body.fares.filter(function(s) { return s.ticket.code.match(/CDS|SDS|SOS/); });
+            if (singles.length) {
+                process.stdout.write(singles.map(fare_list).join(' | ') + ' ');
+                best = singles[0];
+                double = true;
             }
         }
-        if (!best) {
-            best = by_type.CDS || by_type.SDS || by_type.SOS;
-            double = true;
-        }
+
         if (!best) {
             return '-';
         }
+
         var fare = best.adult.fare * (double?2:1);
         if (!(from in store.data)) store.data[from] = {};
         if (!(to in store.data)) store.data[to] = {};
-        var route = prettify(best.route.name);
-        var restriction = best.restriction_code;
-        store.data[from][to] = { fare: fare, route: route };
+        store.data[from][to] = { fare: fare, desc: fare_list(best) };
         var disp = price(fare);
         if (double) disp += ' (2 singles)';
-        if (route != 'Any Permitted') disp += ' (' + route + ')';
-        if (restriction == '2V') {
-            disp += ' (0930+ M-F)';
-        }
         return disp;
     });
 }
 
-var store = { data: {} };
+var store = { data: {}, departure_times: {} };
 
 // First, ask for from and to
 Pprompt([
-    { name: "from", message: "From", validate: function(x) { if (x) return true; return false; } },
-    { name: "to", message: "To", validate: function(x) { if (x) return true; return false; } },
+    { default: 'BRV', name: "from", message: "From", validate: function(x) { if (x) return true; return false; } },
+    { default: 'RDG', name: "to", message: "To", validate: function(x) { if (x) return true; return false; } },
     { name: "day", message: "For the day", type: "confirm" },
-    { name: "time", message: "Departure time", default: "10:00",
+    { name: "time", message: "Departure time", default: "08:01",
         validate: function(x) { if (x.match(/^\d\d:\d\d$/)) return true; return false; }
     },
 ])
@@ -124,6 +164,7 @@ Pprompt([
 .spread(function(from, to) {
     store.from = encodeURIComponent(from.from);
     store.to = encodeURIComponent(to.to);
+    store.departure_times[store.from] = store.time;
 })
 .then(function() {
     console.log(chalk.black('Looking up journey as a whole...'));
@@ -132,13 +173,28 @@ Pprompt([
         Pfetch('http://traintimes.org.uk/' + store.from + '/' + store.to + '/' + store.time + '/monday'),
     ];
 }).spread(function(fare_total, stops) {
-    console.log('Total fare is', chalk.blue(fare_total));
+    console.log('\nTotal fare is', chalk.blue(fare_total));
 
     console.log(chalk.black('Fetching stopping points...'));
 
-    var re = /<a[^>]*href="(\/ajax-stoppingpoints[^"]*)">stops/i;
-    var m = stops.match(re);
-    if (m) {
+    var re = /<li id="result0">[\s\S]*?<li id="result1">/, m;
+    if (m = stops.match(re)) {
+        var res1 = m[0];
+        re = /<strong>.*?(\d\d:\d\d) /;
+        if (m = res1.match(re)) {
+            store.departure_times[store.from] = m[1];
+        }
+        re = /<td>(\d\d:\d\d)<\/td>\s*<td class="origin">.*?<abbr>([A-Z]{3})/g;
+        if (m = res1.match(re)) {
+            m = m.map(function(x){
+                var q = x.match(/<td>(\d\d:\d\d)<\/td>\s*<td class="origin">.*?<abbr>([A-Z]{3})/);
+                store.departure_times[q[2]] = q[1];
+            });
+        }
+    }
+
+    re = /<a[^>]*href="(\/ajax-stoppingpoints[^"]*)">stops/i;
+    if (m = stops.match(re)) {
         return Pfetch('http://traintimes.org.uk' + m[1] + ';ajax=2');
     } else {
         console.log(chalk.red("Could not get stops"));
@@ -147,9 +203,10 @@ Pprompt([
 }).then(function(ints) {
     var stops = [], c = 0;
     ints.tables.forEach(function(x){
-        stops = stops.concat(x.match(/<abbr>[A-Z]{3}/g).map(function(x){ return x.substr(6,3); }));
+        stops = stops.concat((x.match(/<abbr>[A-Z]{3}/g)||[]).map(function(x){ return x.substr(6,3); }));
         stops.push( ints.destination[c++].match(/[A-Z]{3}/)[0] );
     });
+    for (var i in ints.parsed) { store.departure_times[i] = ints.parsed[i]; }
 
     var all_stops = [ store.from ].concat(stops);
     console.log('Stations to consider:', chalk.gray(all_stops));
@@ -163,7 +220,7 @@ Pprompt([
     stop_pairs.forEach(function(pair) {
         result = result.then(function(out) {
             if (out) console.log(chalk.gray(out));
-            process.stdout.write(chalk.gray('  Trying ' + pair[0] + '-' + pair[1] + ': '));
+            process.stdout.write(chalk.gray(pair[0] + '-' + pair[1] + ': '));
             return parse_fare(pair[0], pair[1]);
         });
     });
@@ -198,7 +255,7 @@ Pprompt([
     for (i=0; i<nodes.length-1; i++) {
         var f = nodes[i], t = nodes[i+1],
             d = store.data[f][t];
-        console.log(f + ' ' + chalk.gray('->') + ' ' + t + chalk.gray(': ') + price(d.fare) + ' (' + d.route + ')');
+        console.log(f + ' ' + chalk.gray('->') + ' ' + t + chalk.gray(': ') + d.desc);
         total += d.fare;
     }
     console.log(chalk.green('Total: ' + price(total)));
@@ -209,5 +266,8 @@ Pprompt([
 });
 
 function prettify(s) {
-    return s.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
+    return s
+        .replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();})
+        .replace(/ R$/, ' Return')
+        .replace(/ S$/, ' Single')
 }
