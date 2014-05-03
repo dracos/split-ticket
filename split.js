2c870a66968640bbd5b2bbf9b06ac0e5a8ac5395
Matthew Somerville <matthew-github@dracos.co.uk>
2014-05-03 23:05:21 +0100

    All ticket restrictions from parsed data, and station arrival times too.

--- split.js
+++ split.js
@@ -1,8 +1,4 @@
 #!/usr/bin/env node
-/*
- * TODO
- * Anytime return, not just off-peak
- */
 
 "use strict";
 var inquirer = require("inquirer"),
@@ -12,6 +8,7 @@ var inquirer = require("inquirer"),
     RequestCaching = require("node-request-caching"),
     dijkstra = require("dijkstra"),
     itertools = require("./itertools"),
+    restrictions = require("./restrictions"),
     Q = require("q");
 
 var rc = new RequestCaching({
@@ -39,30 +36,16 @@ function price(n) {
     return printf('£%.2f', n/100).replace('.00', '');
 }
 
-
-function test_restriction(code, time) {
-    if ([ '2V', 'G1', 'BX', 'GF' ].indexOf(code)>-1 && time >= '09:30') {
-        return true;
-    }
-    if ([ 'W3' ].indexOf(code)>-1 && time >= '09:00') { // AND THE 08:59 TILEHURST TO OXFORD!
-        return true;
-    }
-    if ([ 'W4' ].indexOf(code)>-1 && time >= '08:45') { // Additionally valid on the 08:41 departure from Swindon to Cheltenham Spa. 
-        return true;
-    }
-    return false;
-}
-
 function parse_fare(from, to) {
     return Pfetch('http://api.brfares.com/querysimple?orig=' + from + '&dest=' + to)
     .then(function(body) {
-        var b = body.fares.filter(function(s) { return s.ticket.code.match(/SVR|SOR|CDR|SDR|CDS|SDS|SOS/); });
+        //var b = body.fares.filter(function(s) { return s.ticket.code.match(/SVR|SOR|CDR|SDR|CDS|SDS|SOS/); });
 
         function fare_list(s) {
             var o = price(s.adult.fare);
             o += ' (' + prettify(s.ticket.name);
             if (s.route.name != 'ANY PERMITTED') o += ', ' + prettify(s.route.name)
-            if (s.restriction_code != '  ') o += ', ' + s.restriction_code;
+            //if (s.restriction_code != '  ') o += ', ' + s.restriction_code;
             o += ')';
             return o;
         }
@@ -72,35 +55,52 @@ function parse_fare(from, to) {
             if (store.day) {
                 ret = ret || s.ticket.code.match(/CDR|SDR/);
             }
+            ret = ret && restrictions.valid_journey(from, to, store.station_times[from], store.station_times[to], s.restriction_code);
+            //if (s.route.name != 'ANY PERMITTED') return false;
             return ret;
         });
 
-        var best;
+        var best, best_per_route = {};
         returns.forEach(function(r){
+            //if (!restrictions.valid_journey(from, to, store.station_times[from], store.station_times[to], r.restriction_code)) { return; }
+            if (!best || r.adult.fare < best.adult.fare) {
+                best = r;
+            }
+            if (!best_per_route[r.route.name] || r.adult.fare < best_per_route[r.route.name].adult.fare) {
+                best_per_route[r.route.name] = r;
+            }
+            /*
             if (!best) { best = r; return; }
             if (r.adult.fare < best.adult.fare) {
+                best = r; return;
                 if (r.ticket.code == 'SOR' || r.ticket.code == 'SDR') {
                     best = r; return;
                 }
                 if (best.ticket.code == r.ticket.code && r.restriction_code == best.restriction_code) {
                     best = r; return;
                 }
-                if (from in store.departure_times) {
-                    if (test_restriction(r.restriction_code, store.departure_times[from])) {
-                        best = r; return;
-                    }
+                if (restrictions.valid_journey(from, to, store.station_times[from], store.station_times[to], r.restriction_code)) {
+                    best = r; return;
                 }
             }
+            */
         });
+        //console.log(Object.keys(best_per_route));
 
         var double = false;
         if (best) {
             process.stdout.write(returns.map(fare_list).join(' | ') + ' ');
         } else {
             var singles = body.fares.filter(function(s) { return s.ticket.code.match(/CDS|SDS|SOS/); });
-            if (singles.length) {
+            singles.forEach(function(r){
+                if (!restrictions.valid_journey(from, to, store.station_times[from], store.station_times[to], r.restriction_code)) { return; }
+                if (!best) { best = r; return; }
+                if (r.adult.fare < best.adult.fare) {
+                    best = r; return;
+                }
+            });
+            if (best) {
                 process.stdout.write(singles.map(fare_list).join(' | ') + ' ');
-                best = singles[0];
                 double = true;
             }
         }
@@ -119,14 +119,14 @@ function parse_fare(from, to) {
     });
 }
 
-var store = { data: {}, departure_times: {} };
+var store = { data: {}, station_times: {} };
 
 // First, ask for from and to
 Pprompt([
     { default: 'BRV', name: "from", message: "From", validate: function(x) { if (x) return true; return false; } },
     { default: 'RDG', name: "to", message: "To", validate: function(x) { if (x) return true; return false; } },
     { name: "day", message: "For the day", type: "confirm" },
-    { name: "time", message: "Departure time", default: "08:01",
+    { name: "time", message: "Departure time", default: "08:02",
         validate: function(x) { if (x.match(/^\d\d:\d\d$/)) return true; return false; }
     },
 ])
@@ -164,7 +164,7 @@ Pprompt([
 .spread(function(from, to) {
     store.from = encodeURIComponent(from.from);
     store.to = encodeURIComponent(to.to);
-    store.departure_times[store.from] = store.time;
+    store.station_times[store.from] = [ null, store.time ];
 })
 .then(function() {
     console.log(chalk.black('Looking up journey as a whole...'));
@@ -180,15 +180,19 @@ Pprompt([
     var re = /<li id="result0">[\s\S]*?<li id="result1">/, m;
     if (m = stops.match(re)) {
         var res1 = m[0];
-        re = /<strong>.*?(\d\d:\d\d) /;
+        re = /<strong>.*?(\d\d:\d\d) &ndash; (\d\d:\d\d)/;
         if (m = res1.match(re)) {
-            store.departure_times[store.from] = m[1];
+            store.station_times[store.from] = [ null, m[1] ];
+            store.station_times[store.to] = [ m[2], null ];
         }
-        re = /<td>(\d\d:\d\d)<\/td>\s*<td class="origin">.*?<abbr>([A-Z]{3})/g;
+        re = /<td>(\d\d:\d\d)<\/td>\s*<td class="origin">.*?<abbr>([A-Z]{3})[\s\S]*?<td class="destination">.*?<abbr>([A-Z]{3})[\s\S]*?<td>(\d\d:\d\d)/g;
         if (m = res1.match(re)) {
             m = m.map(function(x){
-                var q = x.match(/<td>(\d\d:\d\d)<\/td>\s*<td class="origin">.*?<abbr>([A-Z]{3})/);
-                store.departure_times[q[2]] = q[1];
+                var q = x.match(/<td>(\d\d:\d\d)<\/td>\s*<td class="origin">.*?<abbr>([A-Z]{3})[\s\S]*?<td class="destination">.*?<abbr>([A-Z]{3})[\s\S]*?<td>(\d\d:\d\d)/);
+                if (!store.station_times[q[3]]) store.station_times[q[3]] = [ null, null ];
+                store.station_times[q[3]][0] = q[4];
+                if (!store.station_times[q[2]]) store.station_times[q[2]] = [ null, null ];
+                store.station_times[q[2]][1] = q[1];
             });
         }
     }
@@ -206,7 +210,7 @@ Pprompt([
         stops = stops.concat((x.match(/<abbr>[A-Z]{3}/g)||[]).map(function(x){ return x.substr(6,3); }));
         stops.push( ints.destination[c++].match(/[A-Z]{3}/)[0] );
     });
-    for (var i in ints.parsed) { store.departure_times[i] = ints.parsed[i]; }
+    for (var i in ints.parsed) { store.station_times[i] = ints.parsed[i]; }
 
     var all_stops = [ store.from ].concat(stops);
     console.log('Stations to consider:', chalk.gray(all_stops));
@@ -220,7 +224,7 @@ Pprompt([
     stop_pairs.forEach(function(pair) {
         result = result.then(function(out) {
             if (out) console.log(chalk.gray(out));
-            process.stdout.write(chalk.gray(pair[0] + '-' + pair[1] + ': '));
+            process.stdout.write(chalk.gray(pair[0] + '-' + pair[1] + ' (' + chalk.black(store.station_times[pair[0]]) + ')' + ': '));
             return parse_fare(pair[0], pair[1]);
         });
     });
