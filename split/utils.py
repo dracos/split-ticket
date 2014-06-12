a1fcb69051a5815e6b999b240af328ffc82ab6ef
Matthew Somerville <matthew-github@dracos.co.uk>
2014-06-12 12:27:41 +0100

    Factor parsing ticket types to utils.

--- split/utils.py
+++ split/utils.py
@@ -14,3 +14,31 @@ def fetch(url):
 def price(n):
     if n == '-': return '-'
     return (u'\xa3%.2f' % (n/100)).replace('.00', '')
+
+ticket_types = (
+    'SOS', 'SOR', # Anytime single/return
+    'SDS', 'SDR', # Anytime day
+    'GTS', 'GTR', # Anytime single/return
+    'GPR', # Anytime day return
+
+    'CDS', 'CDR', # Off-peak day
+    'SVS', 'SVR', 'SVH', # Off-peak
+    'G2S', 'G2R', # Offpeak
+
+    'SSS', 'SSR', 'OPS', 'OPR', # Super off-peak single/return
+    'CBB', # Super off-peak single
+    'SOP', # Super off-peak return
+    # Super off-peak day
+    'GDS', 'GDR', 'PDS', 'PDR',
+    'SOA', 'SOB',
+    'AM1', 'AM2',
+    'EGS', 'EGF',
+    'OPD',
+    'SRR', 'SWS', 'SCO', 'C1R', 'CBA',
+
+    # Travelcards
+    #'ADT', # Anytime day
+    #'AM3', 'OLT', 'SOT', 'STO', 'STP', 'WDT', 'WTC', # Super off-peak
+    #'ODT', # Off-peak day
+
+)
